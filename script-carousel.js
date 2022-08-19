1d4a384ed5b41c0a7bf0481f320cee9acf41535c
AIA\E123726 <Alif.Aqil@aia.com>
2022-08-19 12:06:55 +0800

    Final

--- script-carousel.js
+++ script-carousel.js
@@ -1,13 +1,29 @@
+let cw = document.body.clientWidth;
+const tab = document.querySelectorAll(".carousel-tab");
+const sld = document.querySelectorAll(".slide-container");
+const slt = document.querySelector(".slick-track");
+const sd = document.querySelectorAll(".slick");
+const cd = document.querySelectorAll(".card-3");
+const attr = document.getElementsByClassName("carousel-tab active");
+sld.forEach((element)=>{
+    element.style.width = ""+cw+"px" ;
+    console.log(element.style.width)
+})
+slt.style =
+                "opacity: 1; width: "+(5 * cw)+"px; transform: translate3d( " +
+                -(attr[0].attributes[1].value * cw) +
+                "px, 0px, 0px);";
+
 var lastScrollTop = 0;
 window.addEventListener(
   "scroll",
   function () {
     var st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
-      if (document.body.clientWidth <= 748) {
+      if (document.body.clientWidth <= (60/100 * cw)) {
         document.getElementById("nav").className =
           "navbar navbar-default navbar-fixed navbar-fixed-slim navbar-offset-xs";
-      } else if (document.body.clientWidth <= 1000) {
+      } else if (document.body.clientWidth <= (90/100 * cw)) {
         document.getElementById("nav").className =
           "navbar navbar-default navbar-fixed navbar-fixed-slim navbar-offset-sm";
       } else {
@@ -16,7 +32,7 @@ window.addEventListener(
       }
       document.getElementById("sticky").style = "top: 21px";
     } else {
-      if (document.body.clientWidth < 1000) {
+      if (document.body.clientWidth < (90/100 * cw)) {
         document.getElementById("sticky").style = "top: 60px";
       } else {
         document.getElementById("sticky").style = "top: 88px";
@@ -29,16 +45,19 @@ window.addEventListener(
   false
 );
 
-const cd = document.querySelectorAll(".card-3");
 window.addEventListener("resize", function (event) {
+    // slt.style =
+    //             "opacity: 1; width: "+(5 * width)+"px; transform: translate3d( " +
+    //             -(attr[0].attributes[1].value * width) +
+    //             "px, 0px, 0px);";
   if (
-    this.document.body.clientWidth > 480 &&
-    this.document.body.clientWidth < 748
+    this.document.body.clientWidth > (40/100 * cw) &&
+    this.document.body.clientWidth < (60/100 * cw)
   ) {
     cd.forEach((element) => {
       element.style = "border-top-color: rgb(166, 232, 222); height: auto;";
     });
-  } else if (this.document.body.clientWidth < 1000) {
+  } else if (this.document.body.clientWidth < (90/100 * cw)) {
     cd.forEach((element) => {
       element.style =
         "border-top-color: rgb(166, 232, 222); height: 307.521px;";
@@ -53,40 +72,31 @@ window.addEventListener("resize", function (event) {
   }
 });
 
-const tab = document.querySelectorAll(".carousel-tab");
-const slt = document.querySelector(".slick-track");
-tab.forEach((element) => {
-  element.addEventListener("mouseover", (event) => {
-    tab.forEach((item) => {
-      if (item != element) {
-        item.classList.remove("active");
-      } else {
-        element.classList.add("active");
-        let att = element.attributes[1].value;
-        slt.style =
-          "opacity: 1; width: 6320px; transform: translate3d( " +
-          -(att * 1264) +
-          "px, 0px, 0px);";
-      }
-    });
-  });
-});
-
-const sd = document.querySelector(".slick-dots");
-console.log(sd);
-sd.forEach((element) => {
-    element.addEventListener("click", (event) =>{
-        sd.forEach((item) => {
+function carousel(array, array2, e){
+    array.forEach((element) => {
+        element.addEventListener(e, (event) => {
+          array.forEach((item) => {
             if (item != element) {
-              item.classList.remove("slick-active");
+              item.classList.remove("active");
             } else {
-              element.classList.add("slick-active");
+              element.classList.add("active");
               let att = element.attributes[1].value;
+              array2.forEach((el) => {
+                if(el.attributes[1].value == att){
+                    el.classList.add("active");
+                }else{
+                    el.classList.remove("active");
+                }
+            })
               slt.style =
-                "opacity: 1; width: 6320px; transform: translate3d( " +
-                -(att * 1264) +
+                "opacity: 1; width: "+(5 * cw)+"px; transform: translate3d( " +
+                -(att * cw) +
                 "px, 0px, 0px);";
             }
           });
-    });
-});
+        });
+      });
+}
+
+carousel(tab, sd, "mouseover");
+carousel(sd, tab, "click");
